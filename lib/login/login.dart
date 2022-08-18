3df8cf913cdc04c5cfa73b2f77ccd621679381a2
Lovish <2019ucs0093@iitjammu.ac.in>
2022-08-19 00:46:25 +0530

    added splash screen

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,26 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:KneeSupporter/HomePage/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+
 
 class loginPage extends StatelessWidget {
   const loginPage({Key? key}) : super(key: key);
 
   @override
-  // void initState(){
-  //   super.initState()
-  // }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green,
-      body: Center(
-        child: Text("Welcome!",
-          textAlign: TextAlign.center,
-          style: TextStyle(
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: HomePage(),
+      title: Text("Welcome!",
+        textAlign: TextAlign.center,
+        style: TextStyle(
             fontSize: 60,
             fontFamily: "Silkscreen"
-          ),
-        ),
-      ),
+        ),),
+      backgroundColor: Colors.green,
+      loadingText: Text("Loading Data!",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontSize: 20,
+            fontFamily: "Silkscreen"
+        ),),
+      loaderColor: Colors.black,
     );
   }
 }
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:KneeSupporter/HomePage/homepage.dart';
 import 'package:flutter/material.dart';
 import 'login/login.dart';
 
@@ -36,15 +37,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Knee Supporter App"),
-      ),
-    );
-  }
-}
